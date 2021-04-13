22247830bd5ee87d6b99a2203d5fb89cd84efab1
kg4543 <jyc351919@naver.com>
2021-04-13 12:33:46 +0900

    Web DB연결

---
+++ DevHelloAspNet/DbHandlingWebApp/FrmDbConn.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DbHandlingWebApp
+{
+    public partial class FrmDbConn : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnConn_Click(object sender, EventArgs e)
+        {
+            var connString = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                    conn.Open();
+
+                var query = @"Insert Memos 
+                              Values ( '김구겸', 'jyc351919@naver.com', '2개월차 코린이'
+                                , GetDate(), '127.0.0.1')";
+                
+                try
+                {
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.ExecuteNonQuery();
+
+                    LblResult.Text = "데이터 저장 완료";
+                }
+                catch (Exception ex)
+                {
+                    LblResult.Text = $"오류 {ex}";
+                }
+            }
+        }
+    }
+}
