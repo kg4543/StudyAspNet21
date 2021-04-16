08cafad4bbdaa469fd22de02827c31a0fef65288
kg4543 <jyc351919@naver.com>
2021-04-16 16:18:31 +0900

    Search 컨트롤 추가

---
+++ NetNote/NetNote/Controls/CommentControl.ascx.cs
@@ -0,0 +1,44 @@
+﻿using NetNote.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NetNote.Controls
+{
+    public partial class CommentControl : System.Web.UI.UserControl
+    {
+        private DbRepository _repo;
+
+        public CommentControl() 
+        {
+            _repo = new DbRepository();
+        }
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                ctlCommentList.DataSource = _repo.GetNoteComments(Convert.ToInt32(Request["Id"]));
+                ctlCommentList.DataBind();
+            }
+        }
+
+        protected void btnWriteComment_Click(object sender, EventArgs e)
+        {
+            NoteComment comment = new NoteComment();
+            comment.BoardId = Convert.ToInt32(Request["Id"]);
+            comment.Name = txtName.Text;
+            comment.Password = txtPassword.Text;
+            comment.Opinion = txtOpinion.Text;
+
+            _repo.AddNoteComment(comment);
+
+            Response.Redirect($"{Request.ServerVariables["SCRIPT_NAME"]}?Id={Request["Id"]}");
+        }
+    }
+}
