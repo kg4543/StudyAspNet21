05fb903aa2a4c1ff613dbda9a1617e7a2f6a559c
kg4543 <jyc351919@naver.com>
2021-04-20 16:30:11 +0900

    Bootstrap Templete 적용

---
+++ MvcSolutions/Myportpolio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Myportpolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
